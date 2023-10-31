4d52a1d6f8295d8b891df05ce7e09ed0f92b86ce
Danilo Santos <santos.danilo@outlook.com>
2023-10-30 21:30:59 -0300

    Criação menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
 
+export interface Section {
+  order: number,
+  name: string
+  path: string,
+  component: string
+  icone: string,
+  nivel: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,4 +22,50 @@ export class AppComponent {
     console.log("OK!");
     this.matdrawer.toggle();
   }
+
+  menus: Section[] = [
+    {
+      "order": 1,
+      "name": "Inicio",
+      "path": "home",
+      "component": "HomeComponent",
+      "icone": "home",
+      "nivel": 0
+  },
+  {
+    "order": 2,
+    "name": "Painel",
+    "path": "dashboard",
+    "component": "DashboardComponent",
+    "icone": "dashboard",
+    "nivel": 3
+  },
+  {
+    "order": 3,
+    "name": "Cartões",
+    "path": "credit",
+    "component": "CreditComponent",
+    "icone": "credit_card",
+    "nivel": 0
+  },
+  {
+    "order": 4,
+    "name": "Faturas",
+    "path": "invoice",
+    "component": "InvoiceComponent",
+    "icone": "receipt",
+    "nivel": 0
+  },
+  {
+    "order": 5,
+    "name": "Compras",
+    "path": "shopping",
+    "component": "ShoppingComponent",
+    "icone": "shopping_cart",
+    "nivel": 0
+  },
+  ];
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from 'src/template/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { NavbarComponent } from 'src/template/navbar/navbar.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { DatePipe, NgFor } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -28,7 +31,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatToolbarModule,
     MatSidenavModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    NgFor,
+    DatePipe,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
